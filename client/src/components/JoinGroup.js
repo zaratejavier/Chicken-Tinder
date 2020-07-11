f3821ac7af6edfc6390a6b9d9621536c568f1a90
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-07-11 11:14:08 -0600

    working on joingroup users crud

--- client/src/components/JoinGroup.js
+++ client/src/components/JoinGroup.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Axios from "axios"
 
 export default function JoinGroup() {
+    const [user, setUser] = useState("");
+    const [groupCode, setGroupCode] = useState("");
+
+    const handleSubmit = () => {
+        e.preventDefault();
+        Axios.post()
+
+    }
+
+    const addUser = (groupCode) => {
+        
+    }
+
     return (
         <div>
             Join Group
 
-            <form>
-                <label>Input you username</label>
-                <input></input>
-                <label>Input you group code</label>
-                <input></input>
-                <Link to='/Swipe'>Start Swiping!</Link>
+            <form onSubmit={handleSubmit}>
+                <label>Input your username: </label>
+                    <input 
+                        value={user} 
+                        onChange={(e) => setUser(e.target.value)}>
+                    </input>
+                <br />
+                <br />
+                <label>Input your group code: </label>
+                <input 
+                    value={groupCode} 
+                    onChange={(e) => setGroupCode(e.target.value)}>
+                </input>
+                <br />
+                <br />
+                <button>
+                    <Link to='/Swipe'>Start Swiping!</Link>
+                </button>
             </form>
         </div>
     )
