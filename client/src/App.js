4a3810f6b3f9728ffafdb5854f7af5a6b57869a8
Max Sultan <maxsultakent@gmail.com>
2020-07-11 09:53:11 -0600

    react router installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch } from 'react-router-dom';
+import Home from './components/Home';
+import JoinGroup from './components/JoinGroup';
+import CreateGroup from './components/CreateGroup';
+import Swipe from './components/Swipe';
+import Match from './components/Match';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/JoinGroup' component={JoinGroup}/>
+      <Route exact path='/CreateGroup' component={CreateGroup}/>
+      <Route exact path='/Swipe' component={Swipe}/>
+      <Route exact path='/Match' component={Match}/>
+      <Route component={NoMatch} />
+    </Switch>
   );
 }
 
---
+++ client/src/components/JoinGroup.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function JoinGroup() {
+    return (
+        <div>
+            Join Group
+        </div>
+    )
+}
