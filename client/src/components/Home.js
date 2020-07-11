86e9042df79bbb42c3f4a00c365ab16d6dce96cd
Max Sultan <maxsultakent@gmail.com>
2020-07-11 11:12:33 -0600

    added routes and axios call to createa group

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
+
+
 
 export default function Home() {
+    const [groupdCode, setGroupCode] = useState('')
 
-    const CreateAGroup = () => {
-        
+    const generateCode = () => {
+        return `#${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}${Math.floor(Math.random() * 9)}`
+    }
+
+    const createAGroup = () => {
+        console.log('create group')
+        const code = generateCode()
+        console.log(code)
+        Axios.post(`/api/groups`, {name: code})
+        .then(res => {
+            console.log(res)
+            //setGroupCode(res.data.name)
+            //props.location.push(`/CreateGroup`)
+        })
+        .catch(err => console.log(err))
     }
 
     return (
         <div>
             Home
+            <button onClick={() => createAGroup()}>Create a Group</button>
             <button><Link to='/CreateGroup'>Create a Group</Link></button>
             <button><Link to='/JoinGroup'>Join a Group</Link></button>
         </div>
