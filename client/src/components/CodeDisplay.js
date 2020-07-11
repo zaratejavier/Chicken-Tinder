1b25318bf38273483a6e4ea54d9f00ae6581e369
Max Sultan <maxsultakent@gmail.com>
2020-07-11 12:31:55 -0600

    styling for code display, axios call for creating groups

--- client/src/components/CodeDisplay.js
+++ client/src/components/CodeDisplay.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function CodeDisplay(props) {
     return (
-        <div>
+        <div style={{backgroundColor:'#E9692C', textAlign:'center'}}>
             <h1>Your Code is: {props.code}</h1>
         </div>
     )
--- client/src/components/CreateUser.js
+++ client/src/components/CreateUser.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CodeDisplay from './CodeDisplay'
+import Axios from 'axios'
 
 export default function CreateUser(props) {
     const code = props.location.state.code
+    const [username, setUsername] = useState('')
+
+    const handleSubmit = () => {
+        Axios.post(`/api/groups/:group_id/users`)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div>
             {console.log(props.location.state.code)}
             <CodeDisplay code={code}/>
             Create user
+            <form onSubmit={()=> handleSubmit()}>
+                <label> Enter your username</label>
+                <input value={username}
+                onChange={(e) => setUsername(e.target.value)}/>
+                <button type='submit'>Start Swiping</button>
+            </form>
         </div>
     )
 }
