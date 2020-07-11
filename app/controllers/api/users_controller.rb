84dbdc896448215f5451f70da776653d0b790fe6
Lindsay Boyden <lindsayboyden@yahoo.com>
2020-07-11 10:16:24 -0600

    built out users controller and restaurants controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,36 @@
 class Api::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy]
+
+  def index 
+    render json: user.all
+  end 
+
+  def show
+    render json: @user
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else 
+      render json: users.errors, status: 422
+    end  
+  end
+
+  def destroy
+    render json: @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
